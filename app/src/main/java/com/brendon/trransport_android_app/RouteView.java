358888d547820d14bbe0948617b8b2ca20fdc729
Brendon <brennon.mckeever@gmail.com>
2017-04-25 18:20:51 -0500

    Starting on work on route list

---
+++ app/src/main/java/com/brendon/trransport_android_app/RouteView.java
@@ -0,0 +1,16 @@
+package com.brendon.trransport_android_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RouteView extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_view);
+    }
+}
